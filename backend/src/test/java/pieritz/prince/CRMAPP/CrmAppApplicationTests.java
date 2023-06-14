291e902cf9e8a427546b57d8a8626661840c4e54
Prince1337 <prince.pieritz@gmail.com>
2023-06-14 13:45:19 +0200

    feat: Entwicklungsumgebung, DB, CI

---
+++ backend/src/test/java/pieritz/prince/CRMAPP/CrmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pieritz.prince.CRMAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
